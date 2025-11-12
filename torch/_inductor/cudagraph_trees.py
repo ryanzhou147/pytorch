01890ad395c9c7c2973499a939957699a8711d66
Sam Gross <colesbury@gmail.com>
2025-11-12 22:09:43 +0000

    Use self_loc instead of self

--- torch/_inductor/cudagraph_trees.py
+++ torch/_inductor/cudagraph_trees.py
@@ -1444,8 +1444,9 @@ class CUDAGraphNode:
                 self_loc = self_ref()
                 if self_loc is None:
                     return False
-                refcount = self.get_output_refcount(i)
-                if self.cached_tensor_outputs[i]._use_count() > 1:  # pyrefly: ignore
+                refcount = self_loc.get_output_refcount(i)
+                # pyrefly: ignore
+                if self_loc.cached_tensor_outputs[i]._use_count() > 1:
                     # c10::Tensor may also holds one reference count
                     assert refcount >= 3
                     return refcount == 3
