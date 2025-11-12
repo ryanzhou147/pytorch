28984ad18a3b79f3198370de7b81cc24c492d9cb
zpcore <zpcore@gmail.com>
2025-11-12 10:24:29 -0800

    Fix is_shard() only True for Shard
    ghstack-source-id: b271b703a8ae205e1782d04dba6c84770d7c3c20
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167618

--- torch/distributed/tensor/_api.py
+++ torch/distributed/tensor/_api.py
@@ -422,10 +422,13 @@ class DTensor(torch.Tensor):
             placements = list(placements)
             for idx, placement in enumerate(placements):
                 # normalize shard dim to be positive
-                if placement.is_shard():
-                    placement = cast(Shard, placement)
+                if isinstance(placement, Shard | _StridedShard):
                     if placement.dim < 0:
-                        placements[idx] = Shard(placement.dim + local_tensor.ndim)
+                        normalized_dim = placement.dim + local_tensor.ndim
+                        if type(placement) is _StridedShard:
+                            placements[idx] = _StridedShard(normalized_dim, split_factor=placement.split_factor)
+                        elif type(placement) is Shard:
+                            placements[idx] = Shard(normalized_dim)
 
         # `from_local` is differentiable, and the gradient of the dist tensor this function
         # created should flow back the gradients to the local_tensor, so we call an autograd
